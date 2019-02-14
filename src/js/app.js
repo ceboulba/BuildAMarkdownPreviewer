a8d2a70f18388456a3f9043ec587c99cc718bbbd
ceboulba <antoine@archipicture.fr>
2019-02-14 18:41:42 +0100

    ok

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,37 @@
-;(function markdown() {
-  const editorContent = 'aa'
+import marked from 'marked'
+;(function() {
+  const init = `
+  # h1
+  ## h2
+  ### h3
+  * puce
+  1. ol  
+  texte  
+
+  [link](https://www.archipicture.fr)  
+
+  (https://www.archipicture.fr)`
+
   const editor = document.getElementById('editor')
-  const preview = document.getElementById('preview')
+  let preview = document.getElementById('preview')
+  let editorContent = init
+  let previewContent = ''
 
   editor.innerHTML = editorContent
-  preview.innerHTML = editorContent
+  //preview.innerHTML = editorContent
 
-  editor.addEventListener('change', event => {
-    console.log(event.target.innerHTML)
+  editor.addEventListener('keyup', event => {
+    console.log(event.target.value)
+    const input = event.target.value
+    affiche(input)
   })
 
-  function change(content) {
-    return (editorContent = content)
+  function affiche(input) {
+    //previewContent.innerHTML(marked(input))
+    // preview.createElement = `<h1>qsd</h1>`
+    const el = marked(input).toString()
+    preview.innerHTML = el
+    console.log(el)
   }
+  affiche(init)
 })()
